985bf2fbc602b54526a85cb4c4fb6f43bbe940d9
valterUo <valter.uotila@gmail.com>
2019-04-08 22:06:01 +0300

    uploading a file

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const express = require('express')
+const formidable = require('formidable')
+const fs = require('fs')
 const cmd = require('node-cmd')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
-const processRef = cmd.get('sml snippets.sml')
-
+let processRef
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+app.post('/files', (request, response) => {
+  fs.writeFile(request.get('FileName'), request.body, (err) => { 
+     console.log(err)
+     processRef = cmd.get('sml ' + request.get('FileName'))
+     response.send("File created and process started.")
+   })
+ })
 
 app.post('/ml', (request, response) => {
+  if(processRef === undefined) {
+    response.status(400).send({ error: "The ML file has not been defined." });
+  } else {
+  console.log("here")
   let stream = processRef.stdout
   //console.log("Stream starting")
-  processRef.stdin.write(request.body.command + "\n")
+  console.log(request.body)
+  processRef.stdin.write(request.body + "\n")
   stream.once('data', function (data) {
     //console.log("This is the data: ")
-    //console.log(data)
+    console.log(data)
     //console.log("Data ends, data sent.")
     response.send(data)
   })
+}
 })
 
-const PORT = 3002
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
