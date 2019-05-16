079b4489733f817423e39a1a4933590a480dfd6f
valterUo <valter.uotila@gmail.com>
2019-05-16 17:07:32 +0300

    uploading data

--- index.js
+++ index.js
@@ -10,29 +10,76 @@ app.use(cors())
 app.use(bodyParser.text())
 
 app.post('/files', (request, response) => {
-  fs.writeFile(request.get('FileName'), request.body, (err) => { 
-     console.log(err)
-     processRef = cmd.get('sml ' + request.get('FileName'))
-     response.send("File created and process started.")
-   })
- })
+  fs.writeFile(request.get('FileName'), request.body, (err) => {
+    console.log(err)
+    processRef = cmd.get('sml ' + request.get('FileName'))
+    response.send("File created and process started.")
+  })
+})
+
+app.post('/relational', (request, response) => {
+  fs.writeFile(request.get('FileName'), request.body, (err) => {
+    console.log(err)
+    if (processRef === undefined) {
+      response.status(400).send({
+        error: "The ML file has not been defined. The file was not uploaded."
+      })
+    } else {
+      let stream = processRef.stdout
+      processRef.stdin.write("val T'' = " + request.body + "\n")
+      stream.once('data', function (data) {
+        response.send("Relational data file created and the process is using the data.")
+      })
+    }
+  })
+})
+
+app.post('/document', (request, response) => {
+  fs.writeFile(request.get('FileName'), request.body, (err) => {
+    console.log(err)
+    if (processRef === undefined) {
+      response.status(400).send({
+        error: "The ML file has not been defined. The file was not uploaded."
+      })
+    } else {
+      let stream = processRef.stdout
+      processRef.stdin.write("val E'' = " + request.body + "\n")
+      stream.once('data', function (data) {
+        response.send("Document data file created and the process is using the data.")
+      })
+    }
+  })
+})
+
+app.post('/graph', (request, response) => {
+  fs.writeFile(request.get('FileName'), request.body, (err) => {
+    console.log(err)
+    if (processRef === undefined) {
+      response.status(400).send({
+        error: "The ML file has not been defined. The file was not uploaded."
+      })
+    } else {
+      let stream = processRef.stdout
+      processRef.stdin.write("val G'' = " + request.body + "\n")
+      stream.once('data', function (data) {
+        response.send("Graph data file created and the process is using the data.")
+      })
+    }
+  })
+})
 
 app.post('/ml', (request, response) => {
-  if(processRef === undefined) {
-    response.status(400).send({ error: "The ML file has not been defined." });
+  if (processRef === undefined) {
+    response.status(400).send({
+      error: "The ML file has not been defined."
+    });
   } else {
-  console.log("here")
-  let stream = processRef.stdout
-  //console.log("Stream starting")
-  console.log(request.body)
-  processRef.stdin.write(request.body + "\n")
-  stream.once('data', function (data) {
-    //console.log("This is the data: ")
-    console.log(data)
-    //console.log("Data ends, data sent.")
-    response.send(data)
-  })
-}
+    let stream = processRef.stdout
+    processRef.stdin.write(request.body + "\n")
+    stream.once('data', function (data) {
+      response.send(data)
+    })
+  }
 })
 
 const PORT = 3001
